9c7924ac57c5265c0ca9c9e28a4fde049b3ce495
Shiva Nandamuri <shiva.netd@gmail.com>
2025-05-28 20:27:04 +0000

    Migrate build system to Gradle and implement Spring Boot OAuth2 Authorization Server.
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/78998157-8bef-40ad-b043-a8811b5c44cb/be2f3db5-8705-4166-afed-ddc94588ad4d.jpg

---
+++ src/main/java/com/example/oauth2server/entity/User.java
@@ -0,0 +1,21 @@
+package com.example.oauth2server.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(unique = true, nullable = false)
+    private String username;
+    
+    @Column(nullable = false)
+    private String password;
+    
+    private boolean isAdmin;
+}
